1c62e9546776f906bbb03cca449b23c230f3ee49
Richard Wesley <hawkfish@tableau.com>
2019-03-04 11:45:28 -0800

    Remove Tableau copyrights

--- d3/tql.js
+++ d3/tql.js
@@ -1,13 +1,4 @@
 // -----------------------------------------------------------------------------
-//
-// This file is the copyrighted property of Tableau Software and is protected
-// by registered patents and other applicable U.S. and international laws and
-// regulations.
-//
-// Unlicensed use of the contents of this file is prohibited. Please refer to
-// the NOTICES.txt file for further details.
-//
-// -----------------------------------------------------------------------------
 // d3/tql.js
 // -----------------------------------------------------------------------------
 'use strict';
