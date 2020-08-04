a5c296ced0960f73086456c6353c3c5ccf83a4ae
Adrian Vogelsgesang <vogelsgesang@gmail.com>
2020-08-05 01:36:59 +0200

    Enable more lint rules and fix them...
    This commit enables another set of recommended linting rules. Most of
    those rules we already comply with anyway.
    We violated the "no-useless-escapes" rule which is now fixed by this
    commit (https://eslint.org/docs/rules/no-useless-escape).
    Futhermore, we are still violating the "no-prototype-builtins" rule.
    This will be implicitely fixed as we move on to a more typesafe
    usage of TypeScript.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,29 @@
 module.exports = {
     parser: "@typescript-eslint/parser", // Specifies the ESLint parser
     extends: [
+        "eslint:recommended",
         "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin
         "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
         "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     ],
+    env: {
+        node: true,
+        browser: true,
+        mocha: true,
+    },
     parserOptions: {
         ecmaVersion: 2018, // Allow the parsing of modern ECMAScript features
         sourceType: "module", // Allow the use of imports
     },
-    ignorePatterns: ["**/*.min.js"],
     rules: {
         // We don't consider those rules helpful
-        "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/no-empty-function": "off",
         "@typescript-eslint/explicit-module-boundary-types": "off",
         "@typescript-eslint/no-unused-vars": ["error", {vars: "all", argsIgnorePattern: "^_"}],
         // The following rules should be enabled, but aren't yet due to legacy code which still needs
         // to be adapted
         "@typescript-eslint/no-explicit-any": "off",
+        "no-prototype-builtins": "off",
     },
     overrides: [
         {
