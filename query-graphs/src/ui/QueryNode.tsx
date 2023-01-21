33459666ef3b2bf9fce51e9c2d60b14f06bd112d
Adrian Vogelsgesang <avogelsgesang@salesforce.com>
2023-01-21 17:52:45 +0100

    Better layouting of icon

--- query-graphs/src/ui/QueryNode.tsx
+++ query-graphs/src/ui/QueryNode.tsx
@@ -1,18 +1,19 @@
-import {memo} from "react";
-import {Handle, NodeProps, Position} from "reactflow";
-import {TreeNode} from "../tree-description";
+import { memo } from "react";
+import { Handle, NodeProps, Position } from "reactflow";
+import { TreeNode } from "../tree-description";
 import { NodeIcon } from "./NodeIcon";
+import "./QueryNode.css"
 
-function QueryNode({data}: NodeProps<TreeNode>) {
+function QueryNode({ data }: NodeProps<TreeNode>) {
     return (
         <>
             <Handle type="target" position={Position.Top} />
-            <NodeIcon icon={data.symbol} style={{height: "2em"}}/>
-            <div>{data.name}</div>
+            <NodeIcon icon={data.symbol} style={{height: "1.5em"}} />
+            <div style={{textAlign: "center"}}>{data.name}</div>
             <Handle type="source" position={Position.Bottom} />
-        </>
+        </>     
     );
 }
 
 const memoizedQueryNode = memo(QueryNode);
-export {memoizedQueryNode as QueryNode};
+export { memoizedQueryNode as QueryNode };
