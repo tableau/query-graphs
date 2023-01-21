61770a5f9f47a55ecb663c2c7f87da5bbd78356c
Adrian Vogelsgesang <avogelsgesang@salesforce.com>
2023-01-21 17:28:11 +0100

    Port icons to React

--- query-graphs/src/ui/QueryNode.tsx
+++ query-graphs/src/ui/QueryNode.tsx
@@ -1,11 +1,13 @@
 import {memo} from "react";
 import {Handle, NodeProps, Position} from "reactflow";
 import {TreeNode} from "../tree-description";
+import { NodeIcon } from "./NodeIcon";
 
 function QueryNode({data}: NodeProps<TreeNode>) {
     return (
         <>
             <Handle type="target" position={Position.Top} />
+            <NodeIcon icon={data.symbol} style={{height: "2em"}}/>
             <div>{data.name}</div>
             <Handle type="source" position={Position.Bottom} />
         </>
