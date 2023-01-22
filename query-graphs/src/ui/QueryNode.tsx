713a0a29109b417784e9524836f2a0f76565387d
Adrian Vogelsgesang <avogelsgesang@salesforce.com>
2023-01-22 12:53:07 +0100

    Animation for expanding a node (still a bit jiggerish as the layout updates during the animation)

--- query-graphs/src/ui/QueryNode.tsx
+++ query-graphs/src/ui/QueryNode.tsx
@@ -1,39 +1,43 @@
-import {memo, ReactElement, useCallback, useState} from "react";
-import {Handle, NodeProps, Position} from "reactflow";
-import {TreeNode} from "../tree-description";
-import {NodeIcon} from "./NodeIcon";
+import { memo, ReactElement, MouseEvent, useCallback, useState } from "react";
+import { Handle, NodeProps, Position } from "reactflow";
+import { TreeNode } from "../tree-description";
+import { NodeIcon } from "./NodeIcon";
 import "./QueryNode.css";
 
-function QueryNode({data}: NodeProps<TreeNode>) {
+function QueryNode({ data }: NodeProps<TreeNode>) {
     const [expanded, setExpanded] = useState(false);
-    const toggleExpanded = useCallback(() => setExpanded(!expanded), [expanded]);
+    const toggleExpanded = useCallback((e: MouseEvent) => {
+        e.stopPropagation();
+        e.preventDefault();
+        setExpanded(!expanded);
+    }, [expanded]);
 
     const children = [] as ReactElement[];
     for (const [key, value] of (data.properties || []).entries()) {
         children.push(
             <div>
                 <span className="qg-prop-name">{key}:</span> {value}
-            </div>
+            </div>,
         );
     }
 
-    var className="qg-graph-node";
+    let className = "qg-graph-node";
     if (expanded) className += " qg-expanded";
 
     return (
         <>
-        <Handle type="target" position={Position.Top} />
-        <div className={className} onClick={toggleExpanded}>
-            <NodeIcon icon={data.symbol} style={{height: "1.5em"}} />
-            <div style={{textAlign: "center"}}>{data.name}</div>
-            <div className="qg-graph-node-details">
-                {children}
+            <Handle type="target" position={Position.Top} />
+            <div className={className} onClick={toggleExpanded}>
+                <NodeIcon icon={data.symbol} style={{ height: "1.5em" }} />
+                <div style={{ textAlign: "center" }}>{data.name}</div>
+                <div className="qg-graph-node-details">
+                    <div className="qg-graph-node-details-inner">{children}</div>
+                </div>
             </div>
-        </div>
-        <Handle type="source" position={Position.Bottom} />
+            <Handle type="source" position={Position.Bottom} />
         </>
     );
 }
 
 const memoizedQueryNode = memo(QueryNode);
-export {memoizedQueryNode as QueryNode};
+export { memoizedQueryNode as QueryNode };
