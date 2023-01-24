fd6d22bb4640937aa71dfd16647ba8c6bcfd4955
Adrian Vogelsgesang <avogelsgesang@salesforce.com>
2023-01-24 21:09:19 +0100

    Show hint to Ctrl+Click

--- query-graphs/src/ui/QueryNode.tsx
+++ query-graphs/src/ui/QueryNode.tsx
@@ -1,12 +1,12 @@
-import {memo, ReactElement, MouseEvent, useCallback} from "react";
-import {Handle, NodeProps, Position} from "reactflow";
+import { memo, ReactElement, MouseEvent, useCallback } from "react";
+import { Handle, NodeProps, Position } from "reactflow";
 import cc from "classcat";
-import {TreeNode} from "../tree-description";
-import {NodeIcon} from "./NodeIcon";
+import { TreeNode } from "../tree-description";
+import { NodeIcon } from "./NodeIcon";
 import "./QueryNode.css";
-import {useGraphRenderingStore} from "./store";
+import { useGraphRenderingStore } from "./store";
 
-function QueryNode({data, id}: NodeProps<TreeNode>) {
+function QueryNode({ data, id }: NodeProps<TreeNode>) {
     const expanded = useGraphRenderingStore(s => s.expandedNodes[id]);
     const toggleNode = useGraphRenderingStore(s => s.toggleExpandedNode);
     const toggleSubtree = useGraphRenderingStore(s => s.toggleExpandedSubtree);
@@ -21,6 +21,11 @@ function QueryNode({data, id}: NodeProps<TreeNode>) {
         },
         [toggleNode, toggleSubtree, hasChildren, id],
     );
+
+    var expandHint = <></>;
+    if (hasChildren) {
+        expandHint = <div className="qg-click-hint">Ctrl+Click to show all children</div>;
+    }
 
     const children = [] as ReactElement[];
     for (const [key, value] of (data.properties || []).entries()) {
@@ -40,16 +45,20 @@ function QueryNode({data, id}: NodeProps<TreeNode>) {
         },
     ]);
 
+
     return (
         <>
             <Handle type="target" position={Position.Top} />
             <div className={className} onClick={onClick}>
                 <NodeIcon icon={data.icon} iconColor={data.iconColor} />
-                <div className="qg-graph-node-label" style={{background: data.nodeColor}}>
+                <div className="qg-graph-node-label" style={{ background: data.nodeColor }}>
                     {data.name}
                 </div>
                 <div className="qg-graph-node-details nowheel">
-                    <div className="qg-graph-node-details-inner">{children}</div>
+                    <div className="qg-graph-node-details-inner">
+                        {expandHint}
+                        {children}
+                    </div>
                 </div>
             </div>
             <Handle type="source" position={Position.Bottom} />
@@ -58,4 +67,4 @@ function QueryNode({data, id}: NodeProps<TreeNode>) {
 }
 
 const memoizedQueryNode = memo(QueryNode);
-export {memoizedQueryNode as QueryNode};
+export { memoizedQueryNode as QueryNode };
