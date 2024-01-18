e58f707e252824173b677e8dad9589a57be480b6
Adrian Vogelsgesang <avogelsgesang@salesforce.com>
2024-01-18 21:34:32 +0000

    Dump Postgres plans; Remove old Postgres plans
    The old Postgres plans were mostly from older experiments which aren't
    even reproducible on upstream Postgres.
    
    The new Postgres plans are actually dumped through the script in this
    repository.

--- plan-dumper/queries/metadata-describe-table.sql
+++ plan-dumper/queries/metadata-describe-table.sql
@@ -1,5 +1,5 @@
 SELECT c.relchecks, c.relkind, c.relhasindex, c.relhasrules, c.relhastriggers,
-       c.relhasoids, false as relispartition, c.reltablespace,
+       false as relispartition, c.reltablespace,
        CASE WHEN c.reloftype = 0 THEN '' ELSE c.reloftype::pg_catalog.regtype::pg_catalog.text END,
        c.relpersistence
 FROM pg_catalog.pg_class c
