9c27678e6982d9456b470e194278dba17e2e23dc
Adrian Vogelsgesang <avogelsgesang@salesforce.com>
2024-10-19 02:57:36 +0200

    Upgrade dependencies of frontend (#119)

--- query-graphs/src/ui/tree-layout.ts
+++ query-graphs/src/ui/tree-layout.ts
@@ -4,8 +4,7 @@ import * as d3hierarchy from "d3-hierarchy";
 import {NodeDimensions} from "./store";
 import * as treeDescription from "../tree-description";
 import {TreeNode, TreeDescription} from "../tree-description";
-// TODO: import type; fix `prettier` first :/
-import {Edge, Node} from "reactflow";
+import type {Edge, Node} from "reactflow";
 import {assertNotNull} from "../loader-utils";
 import {CSSProperties} from "react";
 
