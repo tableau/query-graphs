dd22027094c9b24f818a899fd7a9981a0d599e4d
Adrian Vogelsgesang <avogelsgesang@salesforce.com>
2026-08-24 10:36:51 -0700

    Add a `analyze-pipelines` plan for Hyper
    This combination was still missing

--- plan-dumper/queries/magicunnesting.sql
+++ plan-dumper/queries/magicunnesting.sql
@@ -1,4 +1,4 @@
--- MODES: analyze, pipelines
+-- MODES: analyze, pipelines, analyze-pipelines
 -- Under pipelines: a correlated scalar subquery is lowered to a magic set; operators are
 -- shared between the magic groupBy and the outer join.
 SELECT o_orderkey, (SELECT SUM(l_quantity) FROM lineitem WHERE l_extendedprice < o_totalprice) FROM orders
